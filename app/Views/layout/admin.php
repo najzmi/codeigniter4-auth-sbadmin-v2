3d7d9525c347f4ec981b98ea2891fb0259171c36
najzmi <najzmitea@yahoo.com>
2025-11-26 18:48:24 +0700

    Mengubah nama assets public

--- app/Views/layout/admin.php
+++ app/Views/layout/admin.php
@@ -9,11 +9,11 @@
 
     <title><?php echo isset($pdn_title) ? $pdn_title	: 'Administrator | Pudin Project'; ?></title>
     <link rel="shortcut icon" href="<?=base_url('assets');?>/img/favicon.ico" type="image/x-icon" />
-    <link href="<?=base_url('assets');?>/vendor/select2/select2.min.css" rel="stylesheet">
-    <link href="<?=base_url('assets');?>/vendor/select2/select2-bootstrap4.min.css" rel="stylesheet">
-    <link href="<?=base_url('assets');?>/vendor/jquery/jquery-ui.min.css" rel="stylesheet">
+    <link href="<?=base_url('assets');?>/gudang/select2/select2.min.css" rel="stylesheet">
+    <link href="<?=base_url('assets');?>/gudang/select2/select2-bootstrap4.min.css" rel="stylesheet">
+    <link href="<?=base_url('assets');?>/gudang/jquery/jquery-ui.min.css" rel="stylesheet">
     <!-- Custom fonts for this template -->
-    <link href="<?=base_url('assets');?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?=base_url('assets');?>/gudang/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -22,7 +22,7 @@
     <link href="<?=base_url('assets');?>/css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- Custom styles for this page -->
-    <link href="<?=base_url('assets');?>/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="<?=base_url('assets');?>/gudang/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     
 
 </head>
@@ -171,23 +171,23 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?=base_url('assets');?>/vendor/jquery/jquery.min.js"></script>
-    <script src="<?=base_url('assets');?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?=base_url('assets');?>/gudang/jquery/jquery.min.js"></script>
+    <script src="<?=base_url('assets');?>/gudang/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?=base_url('assets');?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?=base_url('assets');?>/gudang/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="<?=base_url('assets');?>/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="<?=base_url('assets');?>/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?=base_url('assets');?>/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="<?=base_url('assets');?>/gudang/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?=base_url('assets');?>/gudang/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
     <script src="<?=base_url('assets');?>/js/demo/datatables-demo.js"></script>
     <!-- Notify-->
-    <script src="<?=base_url('assets');?>/vendor/notify/notify.min.js"></script>
+    <script src="<?=base_url('assets');?>/gudang/notify/notify.min.js"></script>
     <script type="text/javascript">
         const flasDatas = "<?= session()->getFlashdata('success');?>";
         if (flasDatas) {
